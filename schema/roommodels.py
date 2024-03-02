295bebf6b3426a60b2e93cc20fb5bf4c4d5802a8
krishpraseeth <krish.praseeth@gmail.com>
2024-03-02 14:13:23 -0800

    adding room models

---
+++ schema/roommodels.py
@@ -0,0 +1,76 @@
+from Exceptions import RoomNotFoundException
+from pymongo import MongoClient
+
+class Messages:
+    def __init__(self, text, timestamp, sender):
+        self.text = text
+        self.timestamp = timestamp
+        self.sender = sender
+
+
+class Rooms:
+    def __init__(self, room_users, messages):
+        self.room_users = room_users
+        self.messages = messages
+
+    @classmethod
+    def from_dict(cls, room_dict):
+        return cls(
+            room_users = room_dict.get("room_users"),
+            messages = [Messages(**msg) for msg in room_dict.get("messages", [])],
+        )
+
+ 
+    def to_dict(self):
+        return {
+                "room_users" : self.room_users,
+                "messages" : {
+                    "text" : self.messages.text,
+                    "timestamp" : self.messages.timestamp,
+                    "sender" : self.messages.sender,
+                }
+            }
+
+
+    def insert_room_info(self):
+        self.client = MongoClient("mongodb+srv://housify-customer-account-test1:housify-customer-test1@directmessages.xzfffoj.mongodb.net/")
+        self.db = self.client.DM
+        self.collection = self.db.DirectMessages
+
+        room_data = self.to_dict()
+        self.collection.insert_one(room_data)
+
+    def retrieve_room_info(self, id = None):
+        self.client = MongoClient("mongodb+srv://housify-customer-account-test1:housify-customer-test1@directmessages.xzfffoj.mongodb.net/")
+        self.db = self.client.DM
+        self.collection = self.db.DirectMessages
+
+        room = self.collection.find_one({"_id": id})
+
+        if not room:
+            raise RoomNotFoundException(f"No Room found with id: {id}")
+        
+        return self.from_dict(room)
+    
+    def retrieve_room_users(self, id = None):
+        self.client = MongoClient("mongodb+srv://housify-customer-account-test1:housify-customer-test1@directmessages.xzfffoj.mongodb.net/")
+        self.db = self.client.DM
+        self.collection = self.db.DirectMessages
+
+        room = self.collection.find_one({"_id": id})
+
+        if not room:
+            raise RoomNotFoundException(f"No Room found with id: {id}")
+        
+        return room.get('room_users', [])
+
+
+    def print_room_info(self):
+        print("Room Information")
+        print(f"Room Users: {self.room_users}")
+        print(f"Message Text: {self.messages.text}")
+        print(f"Message Timestamp: {self.messages.timestamp}")
+        print(f"Sender: {self.messages.sender}")
+        
+
+
